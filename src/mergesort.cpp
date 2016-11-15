85409e3f0b48d2fcd503b5ab88cb23d5bf4f268f
LitvinenkoIra <irairairairairairaira@gmail.com>
2016-11-15 19:02:34 +0200

    addmerge sort implementation

--- src/mergesort.cpp
+++ src/mergesort.cpp
@@ -1,21 +1,18 @@
 #include "include/mergesort.h"
 #include <iostream>
 #include <vector>
-template <typename Tit>
-void print_arr(std::string key, Tit begin, Tit end){
-    std::cout << key << ": ";
-    for(; begin != end; ++begin){
-        std::cout << *begin << " ";
-    }
-    std::cout << std::endl;
-}
+
+//template <typename Tit>
+//void print_arr(std::string key, Tit begin, Tit end){
+//    std::cout << key << ": ";
+//    for(; begin != end; ++begin){
+//        std::cout << *begin << " ";
+//    }
+//    std::cout << std::endl;
+//}
 
 template<typename TIter, typename Cmp>
 void merge (TIter lbegin, TIter lend, TIter rbegin, TIter rend, TIter out, Cmp comparator ){
-    print_arr("merge left", lbegin, lend);
-    print_arr("merge right", rbegin, rend);
-    int length = (lend - lbegin) + (rend - rbegin);
-    TIter bout = out;
     for (; lbegin != lend && rbegin != rend;){
         if (comparator(*lbegin,*rbegin)){
             *out++ = *lbegin++;
@@ -31,13 +28,10 @@ void merge (TIter lbegin, TIter lend, TIter rbegin, TIter rend, TIter out, Cmp c
     if(rbegin != rend){
         std::copy(rbegin, rend, out);
     }
-
-    print_arr("merge out", bout, bout+length);
 }
 
 template<typename TIter, typename Cmp>
 void merge_sort(TIter begin, TIter end, TIter buff, Cmp comparator){
-    print_arr("merge sort ", begin, end);
     if (end - begin == 0)
         return;
     if (end - begin == 1){
@@ -62,7 +56,5 @@ void MergeSort::sort(std::vector<std::string> &lines, CompareType cmp_type){
 
     std::vector<std::string> buff (lines.size());
     merge_sort(lines.begin(), lines.end(), buff.begin(), cmp_strategy_factory_.createCompareStrategy(cmp_type));
-
-
 }
 
